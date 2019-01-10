5b72b0c3ce2f16e097c05b9a5f4e330d2a32b2ef
Andrei Yastrzhembski <andrei.yastrzhembski@gmail.com>
2019-01-10 15:35:40 +0300

    add src/GameEngine.php

---
+++ src/GameEngine.php
@@ -0,0 +1,32 @@
+<?php
+namespace BrainGames\GameEngine;
+
+use function \cli\line;
+use function \cli\prompt;
+
+function runGame($playGame)
+{
+    $NUMBER_OF_ROUNDS = 3;
+
+    line('Welcome to the Brain Games!');
+    line('Answer "yes" if number even otherwise answer "no".' . "\n");
+    $name = prompt('May I have your name?');
+    line("Hello, {$name}!\n");
+
+    for ($i = 0; $i < $NUMBER_OF_ROUNDS; $i++) {
+        $questionAndAnswer = $playGame();
+        $question = $questionAndAnswer['question'];
+        $correctAnswer = $questionAndAnswer['correctAnswer'];
+        line("Question: {$question}");
+        $answer = prompt('Your answer');
+        if ($answer === $correctAnswer) {
+            line('Correct!');
+        } else {
+            line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.");
+            line("Let's try again, {$name}!\n");
+            return;
+        }
+    }
+
+    line("Congratulations, {$name}!\n");
+}
