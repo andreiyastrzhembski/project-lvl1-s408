c53873295ba8cbd801a9384a0e04608101208600
Andrei Yastrzhembski <andrei.yastrzhembski@gmail.com>
2019-01-12 20:27:36 +0300

    refactor

--- src/Games/PlayProgression.php
+++ src/Games/PlayProgression.php
@@ -8,22 +8,25 @@ const MIN_NUMBER = 0;
 const MAX_NUMBER = 20;
 const MIN_STEP = 1;
 const MAX_STEP = 10;
-const PROGRESSION_LENGTH = 9;
+const PROGRESSION_LENGTH = 10;
 
 function playProgression()
 {
     $getQuestionAndAnswer = function () {
         $startNumber = rand(MIN_NUMBER, MAX_NUMBER);
         $step = rand(MIN_STEP, MAX_STEP);
-        $questionPosition = rand(0, PROGRESSION_LENGTH);
+        $questionPosition = rand(0, PROGRESSION_LENGTH - 1);
 
         $progression = [];
-        for ($i = 0; $i <= PROGRESSION_LENGTH; $i++) {
-            $progression[] = $startNumber + ($step * $i);
+        for ($i = 0; $i < PROGRESSION_LENGTH; $i++) {
+            if ($i === $questionPosition) {
+                $correctAnswer = (string) ($startNumber + ($step * $i));
+                $progression[] = '..';
+            } else {
+                $progression[] = $startNumber + ($step * $i);
+            }
         }
 
-        $correctAnswer = (string) $progression[$questionPosition];
-        $progression[$questionPosition] = '..';
         $question = join(' ', $progression);
         
         return [
